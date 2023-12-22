78cb99436a70d92ef763e3ea26fd3a915bd94c12
David Vaughan <dvaug86@gmail.com>
2023-12-22 10:22:32 -0500

    student ranks

---
+++ studentRank.js
@@ -0,0 +1,21 @@
+// Write a class StudentRank with two member variables named students, a string array, and ranks, an integer array. Add a method, highestRank, that returns the highest ranked student. Also, write a lowestRank method which provides the name of the lowest-ranked student. The primary constructor should accept the student and ranks arrays.
+// Example:
+// String[] students = {"Alex", "Chris", "Sam"}
+// int[] ranks = [3, 2, 1]
+// StudentRank sr = new
+// StudentRank(students, ranks);
+
+// sr.highestRank() // it returns "Alex"
+// sr.lowestRank() // it returns "Chris"
+
+// constraints
+// it is guaranteed that each student has a unique rank
+
+// Sample Input for Custom Testing:
+// Taylor, Wesley, Jordan
+// 1, 5, 3
+// Sample output 
+// Wesley, Taylor
+
+// here Wesley has the highest rank of 5,
+// taylor has the lowest rank of 1
